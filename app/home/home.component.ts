cd269ef492080d7073aecb384dc4f37621a9f2fb
Finch <lukaszzieba4@gmail.com>
2016-06-29 21:54:35 +0100

    End ;)

--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -7,11 +7,15 @@ import {OVERLAY_PROVIDERS} from '@angular2-material/core/overlay/overlay';
 
 @Component({
     selector: 'home',
-    templateUrl: 'app/home/home.component.html',   
+    templateUrl: 'app/home/home.component.html',
     providers: [MATERIAL_PROVIDERS],
     directives: [MATERIAL_DIRECTIVES]
 })
-export class HomeComponent implements OnInit {  
-    
-    ngOnInit() { }  
+export class HomeComponent implements OnInit {
+    homeObj: Object = {
+        title: "Angular 2 CRUD app",
+        text: `Learn one way to build applications with Angular and reuse your code and abilities to build apps for any deployment target.
+                    For web, mobile web, native mobile and native desktop.`
+    }
+    ngOnInit() { }
 }
